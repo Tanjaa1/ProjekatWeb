a152fe442281cb409c2dcce7deebab11afd2f90d
milicatosic <milicatosic@uns.ac.rs>
2020-08-20 22:41:05 +0200

    #Konflikt

--- ApartmentReservationProject/src/app/SparkMain.java
+++ ApartmentReservationProject/src/app/SparkMain.java
@@ -2,20 +2,29 @@ package app;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
-import static spark.Spark.staticFiles;
+import static spark.Spark.post;
 
+import static spark.Spark.staticFiles;
+import static spark.Spark.webSocket;
 
 import java.io.File;
 import java.io.IOException;
 
+import com.google.gson.Gson;
+
+import spark.Session;
+
+import beans.User;
+
 
 public class SparkMain {
+	private static Gson g = new Gson();
 
 	public static void main(String[] args) throws IOException {
 		port(9000);
 		
 		staticFiles.externalLocation(new File("./static").getCanonicalPath());
-		
+
 		get("/test", (req, res) -> {
 			return "Works";
 		});
@@ -53,5 +62,6 @@ public class SparkMain {
 			return true;
 		});
 	}
+
 
 }
