940b9c36928eb9f6d1c6d03f4b31de7fbec7d5bb
TanjaaD <ta.drcelic@gmail.com>
2020-08-23 14:41:42 +0200

    spark-login/logout

--- ApartmentReservationProject/static/app/startPage.js
+++ ApartmentReservationProject/static/app/startPage.js
@@ -1,5 +1,19 @@
 Vue.component("start-page", {
-	
+	data: 	function login() {
+			var $form = $("#login");
+			var data = getFormData($form);
+			var s = JSON.stringify(data);
+			$.ajax({
+				url: "login",
+				type:"POST",
+				data: s,
+				contentType:"application/json",
+				dataType:"json",
+				complete: function(data) {
+					$( "#resultLogin" ).html( data.responseText );
+				}
+			});
+		},
 	template: ` 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a href="#" class="navbar-brand">Rezervacije<b>Apartmana</b></a>  		
@@ -18,9 +32,9 @@ Vue.component("start-page", {
 							</div>                            
 							<input type="password" class="form-control" required="required" name="lozinka">
 						</div>
-						<input type="submit" onclick="login()" value="Prijava"> <!-- OVDE JE POZIV FUNKCIJE -->
+						<input type="submit" onclick="login()" value="Prijava">
 					</form>					
-				</div>			
+				</div>	
 				<a href="#" class="btn btn-primary">Regustruj se</a>
 			</div>
 		</div>
